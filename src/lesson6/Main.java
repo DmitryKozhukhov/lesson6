883603caba36f5ad7bb1991a59a3f71531b35a85
DmitryKozhukhov <kodmse@mail.ru>
2021-08-27 22:48:18 +0700

    Homework 6

--- src/lesson6/Main.java
+++ src/lesson6/Main.java
@@ -3,6 +3,14 @@ package lesson6;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Animal Cat = new Cat("Барсик",200, 0);
+        Animal Dog = new Dog("Барбос",500, 10);
+
+        Dog.run(600);
+        Cat.run(150);
+        Dog.swim(9);
+        Cat.swim(10);
+
     }
 }
